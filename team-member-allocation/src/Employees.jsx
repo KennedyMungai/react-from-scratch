82298758b4abdd47154f017c6844322b2ed138aa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-14 15:59:13 +0300

    Added imports for the male profile and the female profile

--- team-member-allocation/src/Employees.jsx
+++ team-member-allocation/src/Employees.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
+import maleProfile from './images/maleProfile.jpg'
+import femaleProfile from './images/femaleProfile.jpg'
 
 
 const Employees = () => {
