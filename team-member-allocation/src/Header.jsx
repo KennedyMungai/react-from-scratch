f7507bea573817f036cd6decc80331bf1d19736a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:24:38 +0300

    Refactored the markup inside the component

--- team-member-allocation/src/Header.jsx
+++ team-member-allocation/src/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Header = ({selectedTeam, teamMemberCount}) => {
   return (
     <header>
-        <h1>Header</h1>
+        <h1>Team Member Allocation</h1>
+        <h3>{selectedTeam} has {teamMemberCount} members</h3>
     </header>
   )
 }
