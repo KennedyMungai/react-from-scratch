3a846692ba234f7403c79bed54f81c1d481e2034
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:49:27 +0300

    Removed the css classes

--- team-member-allocation/src/Header.jsx
+++ team-member-allocation/src/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({selectedTeam, teamMemberCount}) => {
   return (
-    <header className='container bg-black fixed-top mb-4'>
+    <header className='container bg-black'>
         <div className="row justify-content-center mt-4 mb-4 text-white">
           <h1 className='h1'>Team Member Allocation</h1>
           <h3 className='h3'>{selectedTeam} has {teamMemberCount} members</h3>
