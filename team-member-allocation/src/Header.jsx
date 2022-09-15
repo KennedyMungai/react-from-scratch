d4253fcfb5c8ad1ee6f392757d1e97f09f62b0ab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:36:21 +0300

    Made sure the header is fixed to the top

--- team-member-allocation/src/Header.jsx
+++ team-member-allocation/src/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({selectedTeam, teamMemberCount}) => {
   return (
-    <header className='container'>
+    <header className='container fixed-top'>
         <div className="row justify-content-center mt-4 mb-4">
           <h1 className='h1'>Team Member Allocation</h1>
           <h3 className='h3'>{selectedTeam} has {teamMemberCount} members</h3>
