ed100f7891a3fcf8ac2e71b07b83f0f08b52669f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 15:48:19 +0300

    Made the conversion to json data asynchronous

--- meals-spa/src/context.jsx
+++ meals-spa/src/context.jsx
@@ -7,8 +7,8 @@ const AppProvider = ({ children }) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('https://randomuser.me/api');
-                const data = response.json();
+                const response = await fetch('https://randomuser.me/api/');
+                const data = await response.json();
                 console.log(data);
             } catch (error) {
                 console.log(error);
