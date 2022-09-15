047b478ba5cdbb551924c07155410f632c56a22a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:38:36 +0300

    Changed the styling of teh header as well

--- team-member-allocation/src/Header.jsx
+++ team-member-allocation/src/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = ({selectedTeam, teamMemberCount}) => {
   return (
-    <header className='container'>
-        <div className="row justify-content-center mt-4 mb-4">
+    <header className='container bg-black'>
+        <div className="row justify-content-center mt-4 mb-4 text-white">
           <h1 className='h1'>Team Member Allocation</h1>
           <h3 className='h3'>{selectedTeam} has {teamMemberCount} members</h3>
         </div>
