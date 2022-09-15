e9e57552f8350860d647d8bd8614abbda30f817d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 11:13:28 +0300

    Changed the header tag to the main tag

--- team-member-allocation/src/GroupedTeamMembers.jsx
+++ team-member-allocation/src/GroupedTeamMembers.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const GroupedTeamMembers = ({selectedTeam, teamMemberCount}) => {
   return (
-    <header className="container">
+    <main className="container">
         <div className="row justify-content-center mt-3 mb-4">
             <h1>Grouped Team Members</h1>
             <h3>{selectedTeam} has {teamMemberCount} members</h3>
         </div>
-    </header>
+    </main>
   )
 }
 
