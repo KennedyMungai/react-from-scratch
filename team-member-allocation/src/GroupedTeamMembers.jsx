c418f6f625d7587054c402fa8d60290af190c07a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 11:42:26 +0300

    Added logic to the groupTeamMembes function

--- team-member-allocation/src/GroupedTeamMembers.jsx
+++ team-member-allocation/src/GroupedTeamMembers.jsx
@@ -7,7 +7,23 @@ const GroupedTeamMembers = ({ employees, selectedTeam, setTeam }) => {
     const [groupedEmployees, setGroupedEmployees] = useState(groupTeamMembers());
 
     const groupTeamMembers = () => {
-        
+        var teams = [];
+
+        var teamAMembers = employees.filter((employee) => employee.teamName === 'TeamA');
+        var teamA={team: 'TeamA', members: teamAMembers, collapsed: selectedTeam === 'TeamA' ? false: true};
+        teams.push(teamA);
+
+        var teamBMembers = employees.filter((employee) => employee.teamName === 'TeamB');
+        var teamB={team: 'TeamA', members: teamBMembers, collapsed: selectedTeam === 'TeamB' ? false: true};
+        teams.push(teamB);
+
+        var teamCMembers = employees.filter((employee) => employee.teamName === 'TeamC');
+        var teamC={team: 'TeamA', members: teamCMembers, collapsed: selectedTeam === 'TeamC' ? false: true};
+        teams.push(teamC);
+
+        var teamDMembers = employees.filter((employee) => employee.teamName === 'TeamD');
+        var teamD={team: 'TeamA', members: teamDMembers, collapsed: selectedTeam === 'TeamD' ? false: true};
+        teams.push(teamD);
     };
 
     return (
