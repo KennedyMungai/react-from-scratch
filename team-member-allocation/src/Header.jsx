afdf8bbb2317b92e37160e94780eaba70642f1c1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:25:04 +0300

    Added some commas to the component code

--- team-member-allocation/src/Header.jsx
+++ team-member-allocation/src/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Header = ({selectedTeam, teamMemberCount}) => {
   return (
@@ -9,4 +9,4 @@ const Header = ({selectedTeam, teamMemberCount}) => {
   )
 }
 
-export default Header
+export default Header;
