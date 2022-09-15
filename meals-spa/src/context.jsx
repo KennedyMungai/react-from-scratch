3777e6ec4dba603639e262d0030994cc039697a6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 15:31:55 +0300

    Initialized the useEffect hook for react

--- meals-spa/src/context.jsx
+++ meals-spa/src/context.jsx
@@ -4,7 +4,10 @@ import React, { useContext, useEffect } from 'react';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-
+    useEffect(() => {
+        console.log('fetch data here');
+    }, []);
+    
     
     return <AppContext.Provider value={{name: 'john', role: 'student'}}>
         {children}
