3f51d4964d7297a61ba96d11e5cb3adb6f6655e9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 15:52:28 +0300

    Moved the function definitiion inside the react component

--- meals-spa/src/context.jsx
+++ meals-spa/src/context.jsx
@@ -3,17 +3,18 @@ import React, { useContext, useEffect } from 'react';
 
 const AppContext = React.createContext();
 
+const fetchData = async () => {
+    try {
+        const response = await fetch('https://randomuser.me/api/');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const AppProvider = ({ children }) => {
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://randomuser.me/api/');
-                const data = await response.json();
-                console.log(data);
-            } catch (error) {
-                console.log(error);
-            }
-        }
 
         fetchData();
 
