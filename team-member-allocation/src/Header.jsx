37bc70b435ca276b04ca4ea8fbf739cf8737d3b0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:30:54 +0300

    Removed the text-center css class from the div

--- team-member-allocation/src/Header.jsx
+++ team-member-allocation/src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({selectedTeam, teamMemberCount}) => {
   return (
     <header className='container'>
-        <div className="row justify-content-center mt-4 mb-4 text-center">
+        <div className="row justify-content-center mt-4 mb-4">
           <h1 className='h1'>Team Member Allocation</h1>
           <h3 className='h3'>{selectedTeam} has {teamMemberCount} members</h3>
         </div>
