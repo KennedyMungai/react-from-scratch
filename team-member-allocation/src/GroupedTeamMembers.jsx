0ee4f31aff02c6ee1f2afbeab72cacade8194c93
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 11:12:58 +0300

    Added basic markup, styling and logic to the GroupedTeamMembers react component

--- team-member-allocation/src/GroupedTeamMembers.jsx
+++ team-member-allocation/src/GroupedTeamMembers.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const GroupedTeamMembers = () => {
+const GroupedTeamMembers = ({selectedTeam, teamMemberCount}) => {
   return (
-    <div>GroupedTeamMembers</div>
+    <header className="container">
+        <div className="row justify-content-center mt-3 mb-4">
+            <h1>Grouped Team Members</h1>
+            <h3>{selectedTeam} has {teamMemberCount} members</h3>
+        </div>
+    </header>
   )
 }
 
