0bae651adecea1e5f06e3c507e52e21a3da75678
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 11:34:18 +0300

    Initialized the useState hook to the groupedEmployees const

--- team-member-allocation/src/GroupedTeamMembers.jsx
+++ team-member-allocation/src/GroupedTeamMembers.jsx
@@ -4,14 +4,16 @@ import { useState } from 'react';
 
 
 const GroupedTeamMembers = ({ employees, selectedTeam, setTeam }) => {
-  return (
-    <main className="container">
-        <div className="row justify-content-center mt-3 mb-4">
-            <h1>Grouped Team Members</h1>
-            {/* <h3>{selectedTeam} has {teamMemberCount} members</h3> */}
-        </div>
-    </main>
-  )
+    const [groupedEmployees, setGroupedEmployees] = useState(groupTeamMembers());
+
+    return (
+        <main className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+                <h1>Grouped Team Members</h1>
+                {/* <h3>{selectedTeam} has {teamMemberCount} members</h3> */}
+            </div>
+        </main>
+    )
 }
 
 export default GroupedTeamMembers;
