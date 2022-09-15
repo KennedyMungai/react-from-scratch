37643f69ce7d098c081f644d390463c7f0b54763
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:03:32 +0300

    Added a box shadow toggle to the employee card names

--- team-member-allocation/src/Employees.jsx
+++ team-member-allocation/src/Employees.jsx
@@ -123,7 +123,7 @@ const Employees = () => {
                     <div className="card-collection">
                         {
                             employees.map((employee) => {
-                                <div className="card m-2" id={employee.id} style={{cursor:"pointer"}} onClick={handleEmployeeCardClick} >
+                                <div className={(employee.teamName === selectedTeam ? 'card m-2 standount':'card m-2')} id={employee.id} style={{cursor:"pointer"}} onClick={handleEmployeeCardClick} >
                                     {(employee.gender === 'male')?<img src={femaleProfile} alt="Profile" className="card-img-top" /> : <img src={maleProfile} alt="Profile" className="card-img-top" />}
 
                                     <div className="card-body">
