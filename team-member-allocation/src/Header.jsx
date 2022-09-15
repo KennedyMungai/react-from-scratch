82dca1319bb736c068acf9dfd77af88ab90f61e6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:46:58 +0300

    Made the header stick to the top and added a margin bottom of 4

--- team-member-allocation/src/Header.jsx
+++ team-member-allocation/src/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({selectedTeam, teamMemberCount}) => {
   return (
-    <header className='container bg-black'>
+    <header className='container bg-black fixed-top mb-4'>
         <div className="row justify-content-center mt-4 mb-4 text-white">
           <h1 className='h1'>Team Member Allocation</h1>
           <h3 className='h3'>{selectedTeam} has {teamMemberCount} members</h3>
