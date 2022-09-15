4d4889f83b3854fe92b7754dd39064dc6ea3d350
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:27:46 +0300

    Added some bootstrap styling to the header component

--- team-member-allocation/src/Header.jsx
+++ team-member-allocation/src/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header = ({selectedTeam, teamMemberCount}) => {
   return (
-    <header>
-        <h1 className='h3'>Team Member Allocation</h1>
-        <h3 className='h6'>{selectedTeam} has {teamMemberCount} members</h3>
+    <header className='container'>
+        <div className="row justify-content-center">
+          <h1 className='h3'>Team Member Allocation</h1>
+          <h3 className='h6'>{selectedTeam} has {teamMemberCount} members</h3>
+        </div>
     </header>
   )
 }
