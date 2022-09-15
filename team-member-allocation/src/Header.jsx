a40112294484d15b5053e07049d5c8c11131292f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 12:01:37 +0300

    Updated the code for dynamic spelling based on the number of team members

--- team-member-allocation/src/Header.jsx
+++ team-member-allocation/src/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({selectedTeam, teamMemberCount}) => {
     <header className='container bg-black'>
         <div className="row justify-content-center mt-4 mb-4 text-white">
           <h1 className='h1'>Team Member Allocation</h1>
-          <h3 className='h3'>{selectedTeam} has {teamMemberCount} members</h3>
+          <h3 className='h3'>{selectedTeam} has {teamMemberCount === 1 ? "member" : "members"} </h3>
         </div>
     </header>
   )
