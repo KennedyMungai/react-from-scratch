b228f79a3beaa3fb53bc60d9e580c12d016dcca0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-15 10:17:42 +0300

    Added the appropriate props to the Employees arrow function

--- team-member-allocation/src/Employees.jsx
+++ team-member-allocation/src/Employees.jsx
@@ -3,7 +3,7 @@ import maleProfile from './images/maleProfile.jpg'
 import femaleProfile from './images/femaleProfile.jpg'
 
 
-const Employees = () => {
+const Employees = (employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange) => {
     
     return (
         <main className="container">
