0691cfdf492bc9169796cbb75ed9ab494ccef70c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 11:39:20 +0300

    Added some markup, styling and logic to the modal component

--- meals-spa/src/components/Modal.jsx
+++ meals-spa/src/components/Modal.jsx
@@ -5,7 +5,11 @@ import { useGlobalContext } from '../context';
 
 const Modal = () => {
   return (
-    <div>Modal</div>
+    <aside className='modal-overlay'>
+      <div className="modal-container">
+        <h1>Modal Container</h1>
+      </div>
+    </aside>
   )
 }
 
