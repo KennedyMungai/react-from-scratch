6e1744a4cd8184b0803ff9efbf43b9fcd7494a3d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 15:24:14 +0300

    Added the N for AMZN

--- watchlist/src/components/Stocklist.jsx
+++ watchlist/src/components/Stocklist.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 
 const Stocklist = () => {
     const [watchList, setWatchList] = useState([
-        "GOOGL", "MSFT", "AMZ"
+        "GOOGL", "MSFT", "AMZN"
     ]);
 
   return (
