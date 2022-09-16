55b1d3d7343263381656d93af066001bfe4b9b4b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 12:05:45 +0300

    Changed the onClick function call so that the function is not called without being pressed first

--- meals-spa/src/components/Meals.jsx
+++ meals-spa/src/components/Meals.jsx
@@ -24,7 +24,7 @@ const Meals = () => {
         const {idMeal, strMeal: title, strMealThumb:image} = singleMeal;
 
         return <article className='singleMeal' key={idMeal}>
-          <img src={image} className="img" onClick={selectMeal(idMeal)} />
+          <img src={image} className="img" onClick={() => selectMeal(idMeal)} />
           <footer>
             <h5>{title}</h5>
             <button className='like-btn'><BsHandThumbsUp /></button>
