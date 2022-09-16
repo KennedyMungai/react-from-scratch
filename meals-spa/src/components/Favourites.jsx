953b4bd200ac56847c5d897ae733408f8e049367
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 12:55:50 +0300

    Added the markup, styling and logic to the favourites component

--- meals-spa/src/components/Favourites.jsx
+++ meals-spa/src/components/Favourites.jsx
@@ -6,7 +6,23 @@ const Favourites = () => {
   const { favourites, selectMeal, removeFromFavourites } = useGlobalContext();
 
   return (
-    <div>Favourites</div>
+    <section className='favourites'>
+      <div className="favourites-content">
+        <h5>Favourites</h5>
+        <div className="favourites-container">
+          {favourites.map((item) => {
+            const {idMeal, strMealThumb:image} = item;
+
+            return <div key={idMeal} className="favourite-item">
+              <img src={image} className="favourites-img img" />
+              <button className="remove-btn" onClick={()=> removeFromFavourites(idMeal)}>
+                Remove
+              </button>
+            </div>
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
