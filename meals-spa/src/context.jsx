85a2c39d32e69b1e7a9a6c93c23f496276144605
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 12:11:00 +0300

    Set the value of showModal to false

--- meals-spa/src/context.jsx
+++ meals-spa/src/context.jsx
@@ -43,6 +43,10 @@ const AppProvider = ({ children }) => {
         setShowModal(true);
     };
 
+    const closeModal = () => {
+        setShowModal(false);
+    };
+
     useEffect(() => {
         fetchMeals(allMealsUrl);
     }, []);
