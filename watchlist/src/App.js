97d8ed697e8378b387ae1189480dfbce26b2cc64
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 15:03:06 +0300

    Added imports for the StockDetailPage and the StockOverviewPage react components

--- watchlist/src/App.js
+++ watchlist/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import {BrowserRouter, route, routes} from 'react-router-dom';
+import StockDetailPage from './pages/StockDetailPage';
+import StockOverviewPage from './pages/StockOverviewPage';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <routes>
+          <route path='/' elememt={}>
+
+          </route>
+        </routes>
+      </BrowserRouter>
     </div>
   );
 }
