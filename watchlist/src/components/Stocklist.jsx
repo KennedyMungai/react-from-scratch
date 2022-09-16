0ffd69f1c488e4f2b086ab1c6255afda9ad33e3a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 15:34:37 +0300

    Added an import for finnHub from the finnHub js file

--- watchlist/src/components/Stocklist.jsx
+++ watchlist/src/components/Stocklist.jsx
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from 'react';
+import finnHub from '../apis/finnHub';
 
 
 const Stocklist = () => {
     const [watchList, setWatchList] = useState([
         "GOOGL", "MSFT", "AMZN"
     ]);
+
+
 
   return (
     <div>Stocklist</div>
