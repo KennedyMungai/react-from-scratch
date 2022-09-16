32fe02d846878aa598e57b782be31ed899d1e66e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 15:03:56 +0300

    Set the StockOverviewPage as the default page of the App

--- watchlist/src/App.js
+++ watchlist/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <routes>
-          <route path='/' elememt={}>
+          <route path='/' elememt={<StockOverviewPage />}>
 
           </route>
         </routes>
