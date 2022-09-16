4c7b69469ac9c13a22d0432f60797e5798cac2e2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 11:52:07 +0300

    Created the selected meal state

--- meals-spa/src/context.jsx
+++ meals-spa/src/context.jsx
@@ -12,6 +12,7 @@ const AppProvider = ({ children }) => {
     const [loading, setLoading] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
     const [showModal, setShowModal] = useState(false);
+    const [selectedMeal, setSelectedMeal] = useState(null);
 
     const fetchMeals= async (url) => {
         setLoading(true);
