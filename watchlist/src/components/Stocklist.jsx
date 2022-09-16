eabd9b3780a3615a5c2bf3d2c274c57bedc289c2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 15:23:46 +0300

    Capitalized all the hardcoded stocks

--- watchlist/src/components/Stocklist.jsx
+++ watchlist/src/components/Stocklist.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 
 const Stocklist = () => {
     const [watchList, setWatchList] = useState([
-        "googl", "msft", "amz"
+        "GOOGL", "MSFT", "AMZ"
     ]);
 
   return (
