10a1eb48cfcd57d6883aa5c744d89439e2f60d08
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-16 11:17:04 +0300

    Passed down the fetchRandomMeal function to the applicartion

--- meals-spa/src/context.jsx
+++ meals-spa/src/context.jsx
@@ -37,7 +37,7 @@ const AppProvider = ({ children }) => {
     }, [searchTerm]);
     
     
-    return <AppContext.Provider value={{loading, meals, setSearchTerm}}>
+    return <AppContext.Provider value={{loading, meals, setSearchTerm, fetchRandomMeal}}>
         {children}
     </AppContext.Provider>
 }
