06e873a8eda7f67d86e0eb1d375f5b7fdf0e123b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-17 10:39:21 +0300

    Added a return statement before the object

--- watchlist/src/components/Stocklist.jsx
+++ watchlist/src/components/Stocklist.jsx
@@ -24,7 +24,7 @@ const Stocklist = () => {
                 );
 
                 const data = responses.map((response) => {
-                    {
+                    return {
                         data: response.data,
                         symbol: response.config.params.symbol
                     }
