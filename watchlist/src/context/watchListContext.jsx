48517dea0b4df6e048d0efeb290a6980fdcc7077
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-17 15:40:59 +0300

    Defined the watchList state

--- watchlist/src/context/watchListContext.jsx
+++ watchlist/src/context/watchListContext.jsx
@@ -1,9 +1,11 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 
 export const WatchListContext = createContext();
 
 export const WatchListContextProvider = (props) => {
+    const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+
     return (
         <WatchListContext.Provider></WatchListContext.Provider>
     )
