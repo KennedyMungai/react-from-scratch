d36e9f0ba33d8d7e8c237cd36ee877ac4391df12
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-17 15:57:42 +0300

    Adds stock to the watchList. But only if it doesn't exists inside the watchList arraY

--- watchlist/src/context/watchListContext.jsx
+++ watchlist/src/context/watchListContext.jsx
@@ -6,6 +6,12 @@ export const WatchListContext = createContext();
 export const WatchListContextProvider = (props) => {
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
 
+    const addStock = (stock) => {
+        if(watchList.indexOf(stock) === -1) {
+            setWatchList([...watchList, stock]);
+        }
+    };
+
     return (
         <WatchListContext.Provider value={{watchList}}>
             {props.children}
