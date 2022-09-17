0297f9769cdaa2fd3288ad2bdb8f2d3f866d59ff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-17 12:01:28 +0300

    Set the value of the input tag to search

--- watchlist/src/components/Autocomplete.jsx
+++ watchlist/src/components/Autocomplete.jsx
@@ -14,6 +14,7 @@ const Autocomplete = () => {
               type="text" 
               className="form-control"
               autoComplete='off'
+              value={search}
               ></input>
           <label htmlFor='search'>Search</label>
           <ul className='dropdown-menu show'>
