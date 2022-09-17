c20d8f1402c871a99a4f766f97dc5fbccecd66aa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-17 11:53:53 +0300

    Added an input element

--- watchlist/src/components/Autocomplete.jsx
+++ watchlist/src/components/Autocomplete.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 
 const Autocomplete = () => {
   return (
-    <div className='w-50 p-5 rounded mx-auto'>Autocomplete</div>
+    <div className='w-50 p-5 rounded mx-auto'>
+      <div className='form-floating dropdown'>
+        <input 
+              style={{backgroundColor: "rgba(145, 158, 171, .04)"}} 
+              id="search" 
+              placeholder="Search" 
+              type="text" 
+              className="form-control"
+              autoComplete='off'
+              ></input>
+      </div>
+    </div>
   )
 }
 
