84dc66ffbd5891d6f488beddb0694d7ca01479a2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-17 21:04:51 +0300

    Had a definition for one day ago

--- watchlist/src/pages/StockDetailPage.jsx
+++ watchlist/src/pages/StockDetailPage.jsx
@@ -11,6 +11,7 @@ const StockDetailPage = () => {
     const fetchData = async () => {
       const date = new Date();
       const currentTime = Math.floor(date.getTime() / 1000);
+      const oneDay = currentTime - 60*60*24;
 
       const response = await finnHub.get("/stock/candle", {
         params: {
