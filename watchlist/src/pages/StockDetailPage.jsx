f3b03c345c93ec03f70b2822e65d47dbc9b6fb9a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-17 20:54:09 +0300

    Changed the axios import to a finnhub import

--- watchlist/src/pages/StockDetailPage.jsx
+++ watchlist/src/pages/StockDetailPage.jsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
 import React from 'react';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import finnHub from '../apis/finnHub';
 
 
 const StockDetailPage = () => {
