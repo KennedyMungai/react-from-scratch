e11a21ca3381fb3bb262b6f9902e2938dfec1d1f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-18 13:50:36 +0300

    Added chart options for the chart

--- watchlist/src/components/StockChart.jsx
+++ watchlist/src/components/StockChart.jsx
@@ -5,8 +5,30 @@ import Chart from 'react-apexcharts';
 const StockChart = ({ chartData , symbol}) => {
     const {day, week, year} = chartData;
 
+    const options = {
+        title: {
+            text: symbol,
+            align: "center",
+            style: {
+                fontSize: "24px",
+
+            }
+        },
+        chart: {
+            id: "stock data",
+            animations: {
+                speed: 1300
+            }
+        },
+        xaxis: {
+            type: "datetime"
+        }
+    };
+
     return (
-        <div>StockChart</div>
+        <div>
+
+        </div>
     )
 }
 
