826bc3420e38ea4624c64b5cda14227106df1dd1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-18 16:27:47 +0300

    Used axios to retrieve data from the api endpoint inside the useEffect hook

--- watchlist/src/components/StockData.jsx
+++ watchlist/src/components/StockData.jsx
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import finnHub from '../apis/finnHub';
 
 
 const StockData = ({ symbol }) => {
-    useEffect(() => {
+    let isMounted = true;
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await finnHub.get("/stock/profile2", {
+                    params: {
+                        symbol
+                    }
+                });
+                console.log(response);
+            } catch (error) {
+                console.log(error);
+            }
+        }
     }, [symbol])
     
 
