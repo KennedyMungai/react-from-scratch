b73f667f912b0d78c1ded32e8b8aee419b6530e2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-09-18 16:48:38 +0300

    Added something I don't quite understand as of yet

--- watchlist/src/components/StockData.jsx
+++ watchlist/src/components/StockData.jsx
@@ -32,7 +32,25 @@ const StockData = ({ symbol }) => {
 
     return (
         <div>
-
+            {stockData && (
+                <div className='row border bg-white rounded shadow-sm  p-4 mt-5'>
+                    <div className='col'>
+                        <span className='fw-bold'>Name: </span>
+                        <span className='fw-bold'>Country: </span>
+                        <span className='fw-bold'>Ticker: </span>
+                    </div>
+                    <div className='col'>
+                        <span className='fw-bold'>Exchange: </span>
+                        <span className='fw-bold'>Industry: </span>
+                        <span className='fw-bold'>IPO: </span>
+                    </div>
+                    <div className='col'>
+                        <span className='fw-bold'>Market Cap: </span>
+                        <span className='fw-bold'>Shares Outstanding: </span>
+                        <span className='fw-bold'>URL: </span>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
